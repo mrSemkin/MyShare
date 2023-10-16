d5f7d80f489aced64a5393ec2e2a757632e68aad
mr.Sam <73043360+mrSemkin@users.noreply.github.com>
2023-10-16 17:22:41 +0300

    basic navigation

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.index),
-    path('index', views.index),
-    path('about', views.about)
-]
+    path('', views.index, name='home'),
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('general_reports/', views.general_reports, name='general_reports'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('login/', views.login, name='login'),
+    path('user_registration/', views.user_registration, name='user_registration'),
+    path('partners/', views.partners, name='partners'),
+    path('new_partner/', views.new_partner, name='new_partner'),
+    path('fund_project/', views.fund_project, name='fund_project'),
+    path('police/', views.police, name='police'),
+
+]+static(settings.STATIC_URL,document_root=settings.STATIC_URL)
+
--- main/views.py
+++ main/views.py
@@ -2,14 +2,56 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    msg = {'url': 'main/index.html',
+           'msg': 'WELCOME'}
+    return render(request, msg['url'],  msg)
 
 def about(request):
-    # return HttpResponse("<h1>About page</h1")
-    print(request.GET);
-    print(request.POST);
-    ddd = {'name': request.GET.get('name')};
-    return render(request, 'main/about.html', ddd);
+    msg = {'url': 'main/about.html',
+           'msg': 'xx'}
+    return render(request, msg['url'], msg)
+
+def general_reports(request):
+    msg = {'url': 'main/general_reports.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+def feedback(request):
+    msg = {'url': 'main/feedback.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def login(request):
+    msg = {'url': 'main/login.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def user_registration(request):
+    msg = {'url': 'main/user_registration.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def partners(request):
+    msg = {'url': 'main/partners.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def new_partner(request):
+    msg = {'url': 'main/new_partner.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def fund_project(request):
+    msg = {'url': 'main/fund_project.html',
+           'msg': ''}
+    return render(request, msg['url'], msg)
+
+def police(request):
+    msg = {'url': 'main/police.html',
+           'msg': 'po po po '}
+    return render(request, msg['url'], msg)
+
 
 def style(request):
     return
+
+

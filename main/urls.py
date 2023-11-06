0652b166e3ed3f5d34b315eba803a60c18f6f88f
mr.Sam <73043360+mrSemkin@users.noreply.github.com>
2023-11-06 17:38:57 +0200

    edit base_layout.html (#6)
    add to settings saas
    css switching to the bootstup - scss

--- main/urls.py
+++ main/urls.py
@@ -2,20 +2,30 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('index/', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('general_reports/', views.general_reports, name='general_reports'),
-    path('feedback/', views.feedback, name='feedback'),
-    path('login/', views.login, name='login'),
-    path('user_registration/', views.user_registration, name='user_registration'),
-    path('partners/', views.partners, name='partners'),
-    path('new_partner/', views.new_partner, name='new_partner'),
-    path('fund_project/', views.fund_project, name='fund_project'),
-    path('police/', views.police, name='police'),
 
-]+static(settings.STATIC_URL,document_root=settings.STATIC_URL)
+                  path('', views.index, name='home'),
+                  path('index/', views.index, name='index'),
+                  path('about/', views.about, name='about'),
+                  path('general_reports/', views.general_reports, name='general_reports'),
+                  path('feedback/', views.feedback, name='feedback'),
+                  path('partners/', views.partners, name='partners'),
+                  path('new_partner/', views.new_partner, name='new_partner'),
+                  path('fund_project/', views.fund_project, name='fund_project'),
+                  path('police/', views.police, name='police'),
+                  path('user_cabinet/', views.user_cabinet, name='user_cabinet'),
 
+                  path('login/', views.custom_login, name='login'),
+                  path('user_registration/', views.user_registration, name='user_registration'),
+                  path('add_user/', views.add_user, name='add_user'),
+                  # path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+                  path('logout/', views.logout, name='logout'),
+
+                  path('login/guest/', views.login_as_guest, name='login_as_guest'),  # URL для увходу як гість
+
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
